d2ac4e531d1282b5685678b3b98b4bdc769fddb7
unknown <ashikbur007@gmail.com>
2023-07-06 20:08:06 +0600

    component

---
+++ resources/views/pages/projects.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+
+@section('content')
+
+@include('component.projects.-list')
+@include('component.call-action')
+
+@endsection
---
+++ resources/views/pages/resume.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+
+@include('component.education')
+@include('component.professional-skill')
+@include('component.experince')
+@include('component.projects.-list')
+
+@endsection
